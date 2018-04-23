a3159325b53525d63eecde497a7b862c367212ef
chmei <739181006@qq.com>
2018-04-23 23:04:51 +0800

    goods

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -3,12 +3,40 @@
   	<div class="menu-wrapper">
   		<ul class="menu-main">
   			<li v-for="item in goods" class="menu-item">
-  				<span class="text"><span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
+  				<span class="text">
+  					<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
   				{{item.name}}</span>
 			</li>
   		</ul>
   	</div>
-  	<div class="foods-wrapper"></div>
+  	<div class="foods-wrapper">
+  		<ul class="foods-main">
+  			<li v-for="item in goods" class="food-item">
+  				<h3 class="food-title">{{item.name}}</h3>
+  				<div v-for="food in item.foods" class="food-cont">
+  					<div class="avatar">
+        				<img width="64"  height="64" :src="food.image">
+      				</div>
+      				<div class="content">
+        				<div class="name">
+        					{{food.name}}
+        				</div>
+        				<div v-show="food.description!=''" class="description">
+        					{{food.description}}
+        				</div>
+        				<div class="sales-performance">
+        					<span class="salenum">月售{{food.sellCount}}份</span>
+        					<span class="rating">好评率{{food.rating}}%</span>
+        				</div>
+        				<div class="prince">
+        					<span class="nowprince">￥{{food.price}}</span>
+        					<span class="oldprince" v-show="food.oldPrice!=''">￥{{food.oldPrice}}</span>
+        				</div>
+        			</div>
+  				</div>
+  			</li>
+  		</ul>
+  	</div>
   </div>
 </template>
 
@@ -100,6 +128,81 @@ export default {
 	}
 	.foods-wrapper{
 		flex:1;
+
+		.foods-main{
+			.food-item{
+				.food-title{
+					width:100%;
+					height:26px;
+					padding-left:14px;
+					background-color:#f3f5f7;
+					border-left:2px solid #d9dde1;
+					font-size:12px;
+					line-height:24px;
+					font-weight:700px;
+					color:rgb(147,153,159);
+					text-align:left;
+				}
+
+				.food-cont{
+					margin:0 18px;
+					padding:18px 0;
+					font-size:0;
+					border-bottom:1px solid rgba(7,17,27,.1);
+					text-align:left;
+
+					&::last-child{border:none;}
+
+					.avatar{
+						display:inline-block;
+						vertical-align:top;
+					}
+					.content{
+						display:inline-block;
+						margin-left:10px;
+						text-align:left;
+						vertical-align:top;
+
+						.name{
+							font-size:14px;
+							line-height:14px;
+							margin-top:2px;
+							color:#333;
+						}
+						.description{
+							font-size:10px;
+							line-height:10px;
+							margin-top:8px;
+							color:rgb(147,153,159);
+						}
+						.sales-performance{
+							font-size:10px;
+							line-height:10px;
+							color:rgb(147,153,159);
+							margin:8px 0;
+
+							.salenum{margin-right:12px;}
+						}
+						.prince{
+							.nowprince{
+								font-size:14px;
+								line-height:24px;
+								font-weight:700;
+								color:rgb(240,20,20);
+								margin-right:8px;
+							}
+							.oldprince{
+								font-size:10px;
+								line-height:24px;
+								color:rgb(147,153,159);
+								font-weight:700;
+								text-decoration: line-through;
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 }
 </style>
